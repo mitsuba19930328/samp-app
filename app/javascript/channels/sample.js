7b8cfaf19bedd182b7d78ff08c3ff577269b034d
島崎塁 <mitsuba19930328@gmail.com>
2020-11-29 03:31:06 +0900

    javascript practice 90%

--- app/javascript/channels/sample.js
+++ app/javascript/channels/sample.js
@@ -64,4 +64,100 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 
+    //関数について
+    // let createMail = (destination,bill) => {
+    //     let msg = `${destination}様\nお世話になっております。\nPT企画の斉藤です。\n今月の請求額は${bill}円でございます。`;
+    //     console.log(msg);
+    // };
+    // let destination = '田中';
+    // let bill = 5000;
+    // createMail(destination,bill);
+
+    // //まとめ問題②（自分でTRY）
+    // //請求書を送る文章を作る。請求書に必要なのは、宛先、請求額（税金10%として計算）である。
+    // let createMail = (destination,bill) => {
+    //     let msg = `${destination}様\nお世話になっております。\nPT企画の斉藤です。\n今月の請求額は${bill}円でございます。`;
+    //     console.log(msg);
+    // };
+    //
+    // //オーナー情報を準備
+    // let owner1 = { name:'山本', bill:40000, tax:true};
+    // let owner2 = { name:'斉藤', bill:70000, tax:true};
+    // let owners = [owner1,owner2];
+    //
+    // //オーナーへ順次メールを発送（あくまでここではコンソール）
+    // for(let owner of owners){
+    //     let ownerName = owner['name'];
+    //     let ownerBill = owner['bill'];
+    //     if(owner['tax']){
+    //         ownerBill = ownerBill * 1.1;
+    //     }
+    //     createMail(ownerName,ownerBill);
+    // }
+
+    //htmlとの連携（本番の触り）
+    // let elem = document.querySelector('p');
+    // elem.innerText = 'テスト';
+
+    // //独自課題（最初はHello Worldと表示あり。入力フォームがあり、入力ボタンを押すと、Hello Worldが入力フォームに入っていた文字列に変更になる。）
+    // //①changeButtonを押された場合に②の処理が走るようにする
+    // //②フォーム内に記載してある文字列を取得。それをid='hello'に入れるようにする
+    // //以下はそれぞれの要素
+    // let helloText = document.getElementById('hello');
+    // let formText = document.getElementById('inputBox');
+    // let changeButton = document.getElementById('changeButton');
+    //
+    // //以下で実際にHello Textをformに入力された内容に変更します。
+    // changeButton.addEventListener("click", () => {
+    //     helloText.innerText = formText.value;
+    // });
+
+    //総まとめ（自己作成）
+    //文字置換アプリを作成
+    //大きなフォームひとつ、小さなフォーム二つを用意
+    //大きなフォームには原文
+    //小さなフォーム1は変更したい文字を入れる。小さなフォーム2には変更後の文字を入れる。
+    //ボタンを押したら原文の変更したい文字が、変更後の文字になって、原文表示されるようにする。
+
+    let textBody = document.getElementById('inputBox');
+    let textBodyValue = document.getElementById('inputBox').value;
+    let beforeText = document.getElementById('beforeText').value;
+    let afterText = document.getElementById('afterText').value;
+    let changeButton = document.getElementById('changeButton');
+
+    // console.log(textBody.value);
+
+
+    //変更ボタンクリックで発火
+    changeButton.addEventListener("click", () => {
+        //テキスト変更処理を行います
+        let afterTextBodyValue = changeText(textBodyValue,beforeText,afterText);
+        //変更されたテキストをテキストエリアに代入
+        textBody.innerText = afterTextBodyValue;
+        console.log(textBody.innerText);
+    });
+
+    //changeTextで該当文字を検索、あればその部分を変更したい文字に置き換え
+    let changeText = (textBodyValue, beforeText, afterText) =>{
+        if(textBodyValue.includes(beforeText)){
+            //textBodyに検索文字列が入っている場合は置換処理を行う
+            let newTextBodyValue = textBodyValue.replaceAll(beforeText, afterText);
+            return newTextBodyValue;
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
