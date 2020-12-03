acc21823cf5673458f214c576508b0ac5ae8642e
島崎塁 <mitsuba19930328@gmail.com>
2020-12-03 20:36:34 +0900

    i18n file（日本語化ファイル）適用されない問題解決

--- Gemfile
+++ Gemfile
@@ -46,3 +46,4 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'uglifier'
 gem 'jquery-rails'
 gem 'bootstrap'
+gem 'rails-i18n'
--- config/application.rb
+++ config/application.rb
@@ -24,13 +24,7 @@ module SampApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
-
-    # Don't generate system test files.
-    config.generators.system_tests = nil
     config.i18n.default_locale = :ja
+
   end
 end
